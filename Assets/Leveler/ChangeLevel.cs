4465391ebbc511baff0f4fb6312ec8dcee49b8bb
Limitex <76650151+Limitex@users.noreply.github.com>
2021-09-18 23:49:57 +0900

    レベル変更処理

--- Assets/Leveler/ChangeLevel.cs
+++ Assets/Leveler/ChangeLevel.cs
@@ -6,10 +6,28 @@ public class ChangeLevel : MonoBehaviour
 {
     public GameObject Leveler;
     public Vector3 localPosition;
+
+    private ObjectSpawner ob;
+    private float StartSpawnInterval;
+    private float StartObjectDrag;
+
     public int NowLevel { get; private set; } = 0;
+
+    public void Start()
+    {
+        ob = GameObject.Find("ObjectSpawner").GetComponent<ObjectSpawner>();
+        StartSpawnInterval = ob.SpawnInterval;
+        StartObjectDrag = ob.ObjectDrag;
+
+    }
 
     public void Switch()
     {
+        if(ob.SpawnInterval != 0)
+        {
+            ob.SpawnInterval = StartSpawnInterval - (NowLevel / 10f);
+        }
+        ob.ObjectDrag = StartObjectDrag - (NowLevel / 1f);
         var g = Instantiate(Leveler);
         g.GetComponent<Leveler>().Level = ++NowLevel;
         g.transform.parent = transform;
