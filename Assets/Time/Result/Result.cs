69d90601ece0563d2346bf7e693c4ad93c13011c
Limitex <76650151+Limitex@users.noreply.github.com>
2021-09-20 23:37:24 +0900

    フェードインで出現するように変更

--- Assets/Time/Result/Result.cs
+++ Assets/Time/Result/Result.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class Result : MonoBehaviour
 {
@@ -12,40 +13,73 @@ public class Result : MonoBehaviour
     public GameObject GAMEOVER;
     public GameObject YOURTIME;
     public GameObject TIME;
-    // Start is called before the first frame update
+
+    private Image background;
+    private float animartionTime = 0;
+    private float alfa = 0.5f;
+    private float overalfa = 1f;
     void Start()
     {
         Time = CountUP.totalTime;
         Debug.Log(Time);
-        Invoke("Text", WaitTime);
+        //Invoke("Text", WaitTime);
 
+        background = GameObject.Find("ResultBackground").GetComponent<Image>();
+        background.color = new Color(1, 1, 1, 0);
+        GAMEOVER.GetComponent<Image>().color = new Color(1, 1, 1, 0);
+        YOURTIME.GetComponent<Image>().color = new Color(1, 1, 1, 0);
+        TIME.GetComponent<Image>().color = new Color(1, 1, 1, 0);
     }
 
-    // Update is called once per frame
     void Update()
     {
+        background.color = SetColor(background.color.a, alfa, 0.05f);
 
-    }
+        animartionTime += UnityEngine.Time.deltaTime;
 
-
-    void Text()
-    {
-        count++;
-        if (count == 1)
+        if (WaitTime < animartionTime && animartionTime < (WaitTime * 2f))
         {
             GAMEOVER.SetActive(true);
-            Invoke("Text", WaitTime);
+            SetAnimartion(GAMEOVER, overalfa, 0.05f);
         }
-        if (count == 2)
+        else if ((WaitTime * 2f) < animartionTime && animartionTime < (WaitTime * 3f))
         {
             YOURTIME.SetActive(true);
-            Invoke("Text", WaitTime);
+            SetAnimartion(YOURTIME, overalfa, 0.05f);
         }
-        if (count == 3)
+        else if((WaitTime * 3f) < animartionTime && animartionTime < (WaitTime * 4f))
         {
             TIME.SetActive(true);
-
+            SetAnimartion(TIME, overalfa, 0.05f);
         }
     }
+
+    Color SetColor(float a, float b, float f) => new Color(1, 1, 1, Mathf.Lerp(a, b, f));
+
+    void SetAnimartion(GameObject g, float alfa, float val)
+    {
+        var image = g.GetComponent<Image>();
+        image.color = SetColor(image.color.a, alfa, val);
+    }
+
+    //void Text()
+    //{
+    //    count++;
+    //    if (count == 1)
+    //    {
+    //        GAMEOVER.SetActive(true);
+    //        Invoke("Text", WaitTime);
+    //    }
+    //    if (count == 2)
+    //    {
+    //        YOURTIME.SetActive(true);
+    //        Invoke("Text", WaitTime);
+    //    }
+    //    if (count == 3)
+    //    {
+    //        TIME.SetActive(true);
+
+    //    }
+    //}
 }
 
